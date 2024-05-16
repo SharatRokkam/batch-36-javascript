316e52b098d644c329fc97d22524b9e0bd867c44
SharatRokkam <rokkamsharath1@gmail.com>
2024-05-16 15:11:11 +0530

    twoDaysession

---
+++ Day-1/script.js
@@ -0,0 +1,51 @@
+// console.log('working')
+
+// if you create a variable using var, it can redeclared and it can be reintialized
+
+// var firstVariable; //declaration
+// firstVariable = 20; //assigning some value means intialization
+// console.log(firstVariable);
+// var firstVariable = 100; declared and intialized in the same line 
+
+// var second = 10;
+// var second = 20;
+// console.log(second);
+
+// This is not best thing to do as redeclaration and reinitialization can cause errors and confusion...
+
+// let and const - ES6
+
+// What is difference es5 and es6
+// var is introduced in before es6 and let const were introduced in es6
+
+// let and const
+
+//let can be reinitialized and we cannot redeclare it..
+
+// let first = "Hello";
+// let  first = 10;
+
+// console.log(first);
+
+
+// const cannot redeclared and cannot be reinitialized , it should be declared and intialized on the same line.
+
+const a = 'sting';
+// a = 100;
+
+
+console.log(a)
+
+// Variables (foundation in js) - Interview 
+
+// Variable is like a container to store different types of data 
+
+// var , let and const 
+
+// es5 - var 
+// es6 - let and const 
+
+// camel case 
+var myVariable = 100;
+// pascal case 
+
---
+++ Day-2/second.js
@@ -0,0 +1,54 @@
+// console.log('working');
+
+// data types in js
+
+// Number
+// let first = 10;
+// let secondVar = 34.4;
+// let third = -111;
+
+// console.log(third);
+
+// String - single quotation, double quotation and backtick
+
+// let firstString = "screen sharing";
+
+// alert(firstString)
+
+
+// boolean 
+
+
+// let a = 10;
+// let b = 1;
+
+// console.log(a == b);
+
+
+// BigInt 
+
+// let bigNum = BigInt(19294274140724701741470174017401704710470174017047104710470174017407070174);
+// console.log(bigNum)
+
+
+// undefined 
+
+// var a ;
+// console.log(a);
+
+
+// Null and Symbol 
+
+// let a = null;
+
+
+// Symbol  - es6
+// holds value that  have different instances(example) and memory allocated to it  
+//returns new unique symbol value
+
+
+let first = Symbol(10);
+let secondVar = Symbol(10)
+
+console.log(first == secondVar);
+
