9c7dac194f3a49ef11df3bcdfa6b6f3e20ff3ea8
SharatRokkam <rokkamsharath1@gmail.com>
2024-05-27 15:23:31 +0530

    Number and String Methods

---
+++ Day-9/script.js
@@ -0,0 +1,79 @@
+// console.log('good morning');
+
+// Today we will be learning about Number, and String Methods 
+
+//  const firstNumber = 10000000000;
+
+//  const newNumber = new Number(30);
+
+
+//  console.log(typeof firstNumber);
+
+//  console.log(newNumber);
+
+//  console.log(newNumber.valueOf());
+
+// console.log(firstNumber.toFixed(2));
+
+// console.log(newNumber.toString());
+
+// console.log(firstNumber.toPrecision(1));
+
+// console.log(firstNumber.toLocaleString('en-IN'));
+
+
+// let singleQuotes = 'first,,  string';
+
+// let doubleQuotes = "second, string"
+
+// let backTick = `third string`;
+
+// console.log(singleQuotes.length);
+// console.log(backTick.length);
+
+// let str1 = 'Hello';
+// let str2 = 'World';
+
+// console.log(str1 + ', ' + str2);
+
+// or you can use concat method 
+
+// console.log(str1.concat(', ' , str2));
+
+// let accessingFirst = str1[3];
+// console.log(str2.charAt(2));
+
+// console.log(str1.toUpperCase());
+// console.log(str1.toLowerCase());
+
+// console.log(str1 + str2.replace('World', ' Universe'));
+
+
+// console.log(str1.indexOf('l'));
+
+// console.log(str1.substring(1,4));
+
+// const newVariable = 'This will have substring';
+
+// let extractingValue = newVariable.substring(15, 24);
+// console.log(extractingValue);
+
+// let lengthyString = 'apple|banana|mango';
+
+// console.log(lengthyString);
+// console.log(lengthyString.trimStart());
+
+// console.log(lengthyString.includes('w'));
+
+// console.log(lengthyString.startsWith("H"));
+
+// console.log(lengthyString.slice(-13));
+
+// const myArray = lengthyString.split('|');
+// console.log(myArray[2]);
+
+
+// console.log(lengthyString);
+
+
+
